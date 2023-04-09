6cc8c77c2a4f0e29cbed0d1afe521288410979f4
Rey Garcia <jreygarcia24@gmail.com>
2023-04-09 16:08:40 +0800

    UI for results-summary component

--- app.vue
+++ app.vue
@@ -8,33 +8,41 @@ const { summaryItems } = useSummary();
     <div class="container">
       <!-- Your Result -->
       <div class="results">
-        <div>Your Result</div>
-        <div>
-          <span>76</span>
-          <span>of</span>
-          <span>100</span>
-        </div>
-        <div>
-          Great
-        </div>
-        <div>
-          You scored higher that 65% of the people who have taken these tests.
-        </div>
-      </div>
-      <!-- Summary -->
-      <div class="summary">
-        <div>Summary</div>
-        <div>
-          <div v-for="({ category, icon, score }, index) in summaryItems" :key="index">
-            <div>
-              <span><img :src="icon" alt=""></span>
-            </div>
-            <div>
-              <span>{{ category }}</span>
-              <span>{{ score }}</span>
+        <h1>Your Result</h1>
+        <!-- Make this container a circle -->
+        <div class="results__display__container">
+          <div class="results__display">
+            <div class="results__numerator">76</div>
+            <div class="results__denominator">
+              <p>of 100</p>
             </div>
           </div>
         </div>
+        <h1>
+          Great
+        </h1>
+        <p>
+          You scored higher that 65% of the people who have taken these tests.
+        </p>
+      </div>
+      <!-- Summary -->
+      <div class="summary">
+        <h1 class="summary__title">Summary</h1>
+        <div class="summary__item__container">
+          <div v-for="({ category, icon, score }, index) in summaryItems" :key="index" :class="`summary__item summary__item__${category}`">
+            <img class="summary__item__icon" :src="icon" alt="">
+            <div class="summary__item__details">
+              <div>
+                <span class="summary__item__category">{{ category }}</span>
+              </div>
+              <div class="summary__item__score">
+                <span class="summary__item__user-score">{{ score }}</span><span> / 100</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button class="summary__button">Continue</button>
       </div>
     </div>
   </div>
@@ -42,19 +50,11 @@ const { summaryItems } = useSummary();
 
 <style lang="scss" scoped>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css');
-.container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 1rem;
-}
+
 .main {
   display: grid;
   justify-content: center;
   align-content: center;
-  height: 100vh;
-}
-.results {
-  border: 1px solid #ccc;
-  border-radius: 1rem;
+  min-height: 100vh;
 }
 </style>
